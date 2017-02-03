15f72a1f687c33a7dfe4459c8967974a0b284af9
聂路 <nielumartin@hotmail.com>
2017-02-03 08:20:16 +0800

    test for template&route

--- test/test_server.py
+++ test/test_server.py
@@ -3,18 +3,33 @@
 
 from creadr.app import app
 import unittest
+import json
 
 
 class MainTestCase(unittest.TestCase):
 
-    def test_index(self):
+    def test_templates(self):
         app.testing = True
         client = app.test_client()
 
         r = client.get('/')
         assert r.status_code == 200
-        assert 'Hello World' in r.data.decode('utf-8')
-        print 'hello'
+        assert 'Pretty Cool' in r.data.decode('utf-8')
+
+        r = client.get('/notExistURL')
+        assert r.status_code == 404
+        assert '404' in r.data.decode('utf-8')
+        # print 'templates ok'
+
+    def test_route(self):
+        app.testing = True
+        client = app.test_client()
+
+        r = client.get('/api')
+        assert isinstance(json.loads(r.data.decode('utf-8')), (object))
+        # print 'route ok'
+
+
 
 if __name__ == '__main__':
     unittest.main()
