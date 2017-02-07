3d32b12661755c49b4a53d3f0a00f9e1e935a3a5
lizekang <zekangli@hustunique.com>
2017-02-07 14:36:05 +0800

    update of split of Chinese and English

--- creadr/creadr_text_processing.py
+++ creadr/creadr_text_processing.py
@@ -1,5 +1,5 @@
 # -*- coding=UTF-8 -*-
-#import split_zh_en
+import split_zh_en
 from pypinyin import pinyin
 import jieba.posseg as pseg
 
@@ -7,22 +7,24 @@ import jieba.posseg as pseg
 def cut(text):
     """demo one way to encapsulate the text with metadata, also display"""
     result = []
-   # text = split_zh_en.split_zh_en(text)
-    for token in pseg.cut(text):
-        text_obj = {}
-        word, nature = token
-        pins = pinyin(word)
-        text_obj['word'] = word
-        text_obj['nature'] = nature
-        text_obj['pinyins'] = pins
-        result.append(text_obj)
+    text1 = split_zh_en.split_zh_en(text)
+    for txt in text1:
+        for token in pseg.cut(txt):
+            text_obj = {}
+            word, nature = token
+            if nature != "x":
+                pins = pinyin(word)
+                text_obj['word'] = word
+                text_obj['nature'] = nature
+                text_obj['pinyins'] = pins
+                result.append(text_obj)
     return result
 
 texts = [
-        u'某君昆仲，今隐其名，皆余昔日在中学时良友；分隔多年，消息渐阙。日前偶闻其一大病；适归故乡，迂道往访，则仅晤一人，言病者其弟也。劳君远道来视，然已早愈，赴某地候补⑵矣。因大笑，出示日记二册，谓可见当日病状，不妨献诸旧友。持归阅一过，知所患盖“迫害狂”之类。语颇错杂无伦次，又多荒唐之言；亦不著月日，惟墨色字体不一，知非一时所书。间亦有略具联络者，今撮录一篇，以供医家研究。记中语误，一字不易；惟人名虽皆村人，不为世间所知，无关大体，然亦悉易去。至于书名，则本人愈后所题，不复改也。七年四月二日识。'
+u'需要注意的是，虽然对str调用encode()方法是错误的，但实际上Python不会抛出异常，而是返回另外一个相同内容但不同id的str；对unicode调用decode()方法也是这样。很不理解为什么不把encode()和decode()分别放在unicode和str中而是都放在basestring中，但既然已经这样了，我们就小心避免犯错吧。'
 ]
 for text in texts:
     obj = cut(text)
     print()
-    print (obj)
-    print ('\n\n')
+    print(obj)
+    print()
--- creadr/split_zh_en.py
+++ creadr/split_zh_en.py
@@ -52,6 +52,3 @@ def is_zh(c):
 
     else:
         return False
-
-text = u'需要注意的是，虽然对str调用encode()方法是错误的，但实际上Python不会抛出异常，而是返回另外一个相同内容但不同id的str；对unicode调用decode()方法也是这样。很不理解为什么不把encode()和decode()分别放在unicode和str中而是都放在basestring中，但既然已经这样了，我们就小心避免犯错吧。'
-print(split_zh_en(text))
