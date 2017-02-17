266072298c94243c1373be247dfe1e4f1a00d304
lizekang <zekangli@hustunique.com>
2017-02-17 23:40:43 +0800

    add annotations and improve test

--- creadr/creadr_text_processing.py
+++ creadr/creadr_text_processing.py
@@ -1,12 +1,19 @@
-# -*- coding=UTF-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from creadr.split_zh_en import split_zh_en
 from creadr.text_class import AnalyzedWord
-from pypinyin import pinyin
 import jieba.posseg as pseg
 
 
 def cut(text):
-    """demo one way to encapsulate the text with metadata, also display"""
+    """demo one way to encapsulate the text with metadata, also display
+    Args:
+        text: user request str
+
+    Returns:
+        An array contains every word's instance, contains word, pinyin, nature
+    """
+
     result = []
     text1 = split_zh_en(text)
     for txt in text1:
@@ -18,11 +25,15 @@ def cut(text):
     return result
 
 texts = [
-u'需要注意的是，虽然对str调用encode()方法是错误的，但实际上Python不会抛出异常，而是返回另外一个相同内容但不同id的str；对unicode调用decode()方法也是这样。很不理解为什么不把encode()和decode()分别放在unicode和str中而是都放在basestring中，但既然已经这样了，我们就小心避免犯错吧。'
+'需要注意的是，虽然对str调用encode()方法是错误的，但实际上Python不会抛出异常，而是返回另外一个相同内容但不同id的str；对unicode调用decode()方法也是这样。很不理解为什么不把encode()和decode()分别放在unicode和str中而是都放在basestring中，但既然已经这样了，我们就小心避免犯错吧。'
 ]
-for text in texts:
-    objs = cut(text)
-    result = []
-    for obj in objs:
-        result.append(obj.to_json())
-    print(result)
+def print_data_in_json():
+    """ demo data encapsulated in class in json array"""
+    for text in texts:
+        objs = cut(text)
+        result = []
+        for obj in objs:
+            result.append(obj.to_json())
+        print(result)
+
+print_data_in_json()
--- creadr/text_class.py
+++ creadr/text_class.py
@@ -1,18 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from pypinyin import pinyin
 
 class AnalyzedWord(object):
+    """Summary of class here.
+    Encapsulate word, pinyin, nature in class
+
+    Attributes:
+        word: Chinese words
+        nature: the word's nature
+        pinyin: the word's pinyin
+    """
     def __init__(self, word, nature):
         self.word = word
         self.nature = nature
         self.pinyin = self.get_pinyin(word)
 
     def get_pinyin(self, word):
+        """use pypinyin to get pinyin of word"""
         pins = pinyin(word)
         return pins
 
     def to_json(self):
+        """demo one way to display word, pinyin and nature in json"""
         return {
-            'word': self.word,
+            'word': self.word.encode('utf-8'),
             'pinyin': self.pinyin,
             'nature': self.nature
         }
