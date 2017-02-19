986a8951bd1ddfad08a518019c54005ad9f44cbf
MartinNey <nielumartin@hotmail.com>
2017-02-19 18:33:18 +0800

    fix spell problem

--- test/test_text_processing.py
+++ test/test_text_processing.py
@@ -46,5 +46,5 @@ def test_cut():
         result = cut(text)
         for obj, obj_expected in six.moves.zip(result, expected):
             assert obj['word'] == obj_expected.word
-            assert obj.['pinyin'] == obj_expected.pinyin
-            assert obj.['nature'] == obj_expected.nature
+            assert obj['pinyin'] == obj_expected.pinyin
+            assert obj['nature'] == obj_expected.nature
