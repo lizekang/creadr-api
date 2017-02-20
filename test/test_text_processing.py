254c98b9be4c434c4771beaa83b6b54eac86730b
ZekangLi <zekangli@hustunique.com>
2017-02-19 23:10:01 -0600

    Update test_text_processing.py

--- test/test_text_processing.py
+++ test/test_text_processing.py
@@ -46,8 +46,5 @@ def test_cut():
         result = cut(text)
         for obj, obj_expected in six.moves.zip(result, expected):
             assert obj['word'] == obj_expected.word
-<<<<<<< HEAD
-=======
             assert obj['pinyin'] == obj_expected.pinyin
             assert obj['nature'] == obj_expected.nature
->>>>>>> creadr/master
